89c78622cb1aeddbd7277a5bebbce0c0476bf246
Bhimarao Kulkarni <69769799+bhimaraok@users.noreply.github.com>
2024-09-01 10:24:48 +0000

    save

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header"
+import Body from "./components/Body"
+import Footer from "./components/Footer"
+
+
+function App() {
+  
+  return (
+    <>
+      <div className="">
+     
+
+      </div>
+    </>
+  )
+}
+
+export default App
